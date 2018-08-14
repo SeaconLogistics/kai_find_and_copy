6bc7d162be1a6de3f564d7b1d55bc5f6c4367e87
Kai-Arne Reiter <git@mirrorspock.com>
2018-08-14 13:44:11 +0200

    added feedback when no files found, and stop searching when a file is found.

--- kai_find_and_copy.sh
+++ kai_find_and_copy.sh
@@ -11,10 +11,24 @@ targetpath="/mnt/seacon-share/Kewill/ashland/in/"
 if [ $HOSTNAME = "Kai-Arnes-MacBook-Pro.local" ]; then
   echo "hi Kai on $HOSTNAME"
   searchterm="4559340"
+  # searchterm="onzin"
   searchpath="./search_dir/*.edi"
   targetpath="./target_dir/"
 fi
 
 echo "searching for $searchterm in $searchpath"
 
-cp `grep -l $searchterm $searchpath | xargs -0 -I'{}'` $targetpath
+foundfilescount=$(grep -l $searchterm $searchpath|wc -l|sed 's/ //g')
+
+case  "$foundfilescount" in
+0)
+  echo "No files found"
+  read -p "Press enter to continue"
+  ;;
+*)
+  echo "Found: -$foundfilescount- copying first one only"
+  read -p "Press enter to continue"
+  cp `grep -l -m1 $searchterm $searchpath` $targetpath
+  ;;
+esac
+
